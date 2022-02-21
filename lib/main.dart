bcfc555b14cf68cbbbf99f7104ac275c5510ab70
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2022-02-21 17:02:54 +0530

    Item add and item display success

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,76 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: Colors.blue,
         accentColor: Colors.orange),
+    home: MyApp(),
   ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List todos = List();
+
+  String input = "";
+
+  @override
+  void initState() {
+    super.initState();
+    todos.add("item1");
+    todos.add("item2");
+    todos.add("item3");
+    todos.add("item4");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("mytodos"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Add TodoList"),
+                  content: TextField(
+                    onChanged: (String value) {
+                      input = value;
+                    },
+                  ),
+                  actions: [
+                    FlatButton(
+                        onPressed: () {
+                          setState(() {
+                            todos.add(input);
+                          });
+                        },
+                        child: Text("Add"))
+                  ],
+                );
+              });
+        },
+      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                key: Key(todos[index]),
+                child: Card(
+                  child: ListTile(
+                    title: Text(todos[index]),
+                  ),
+                ));
+          }),
+    );
+  }
 }
